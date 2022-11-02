4ab31fbf543d5c58f2423be0aac7b1d1dfa54433
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-11-02 08:52:09 +0300

    Написал решение первой задачи домашнего заданияпо к первому семинару

--- Work1/HTask1/Program.cs
+++ Work1/HTask1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее.
+// Например:
+// а=5; b=7 -> max=7
+// а=2; b=10 -> max=10
+// а=-9; b=-3 -> max=-3
+Console.Clear();
+Console.WriteLine ("Введите первое число: ");
+int numberOne = int.Parse ( Console.ReadLine ());
+Console.WriteLine ("Введите второе число: ");
+int numberTwo = int.Parse ( Console.ReadLine ());
+if (numberOne>numberTwo)
+{
+    Console.WriteLine ($"Большее число max = {numberOne} ");
+}
+else
+{
+  Console.WriteLine ($"Большее число max = {numberTwo} ");  
+}
