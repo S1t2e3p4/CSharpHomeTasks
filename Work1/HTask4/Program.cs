299edd1651b6681174c5afe35dc12aa3eb1e3227
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-11-02 15:02:42 +0300

    Написал решение четвёртой задачи домашнего заданияпо к первому семинару

---
+++ Work1/HTask4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, котрая принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// Например:
+// 5 -> 2,4
+// 8 -> 2,4,6,8
+ Console.Clear();
+ Console.WriteLine("Введите число: ");
+ int number = int.Parse(Console.ReadLine());
+ if (number>1)
+ {
+    int N=2;
+    while (N<=number)
+    {
+    Console.Write (N);
+    Console.Write (", ");
+    N=N+2;
+    }
+ }
+    else
+    {
+        int N = -2;
+        while (N>=number)
+        {
+        Console.Write (N);
+        Console.Write (", ");
+        N=N-2;
+        }
+    }
