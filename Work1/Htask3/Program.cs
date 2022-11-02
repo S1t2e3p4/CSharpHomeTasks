e44bfc7f9c1ce22996aee9e98c55eee34f00e5bf
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-11-02 14:35:34 +0300

    Написал решение третьей задачи домашнего заданияпо к первому семинару

---
+++ Work1/Htask3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, котрая принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// Например:
+// 4 -> Да
+// -3 -> Нет
+// 7 -> 2Нет
+ Console.Clear();
+        Console.WriteLine("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+        if (number>0)
+        {
+        int count = number/2;
+        if ((count*2) < number)
+            {
+            Console.WriteLine ($"Число {number} - Нечётное");
+            }
+        else
+        {
+         Console.WriteLine ($"Число {number} - Чётное");
+        }
+        }
+        else
+        {
+          int count = number/2;
+        if ((count*2) > number)
+            {
+            Console.WriteLine ($"Число {number} - Нечётное");
+            }
+        else
+        {
+         Console.WriteLine ($"Число {number} - Чётное");
+        }  
+        }
+    
+        
