d537e418a197f1433d50cd97a12958d39a5cf177
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-11-04 13:49:30 +0300

    Написал решение задачи1 по второму семинару

--- Work2/Htask21/Program.cs
+++ Work2/Htask21/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// Например:
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Clear();
+ Console.WriteLine("Введите трёхзначное число: ");
+ int number = int.Parse(Console.ReadLine());
+ if ((number>99&&number<1000)||(number<-99&&number>-1000))
+ {
+ int Ostatok = number%100;
+ int betveenCount = Ostatok/10;
+ if (betveenCount<0)
+ {
+    betveenCount=betveenCount*-1;
+ }
+ Console.WriteLine (betveenCount);
+ }
+ else
+ {
+    Console.WriteLine ("Ошибка! Введено некорректное значение.");
+ }
+ 
