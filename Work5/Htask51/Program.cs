d21dfbb2bce6bfd95ffa90878352115b617202de
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-11-04 18:19:55 +0300

    Написал решение задачи5.2 по пятому семинару

--- Work5/Htask51/Program.cs
+++ Work5/Htask51/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// Например:
+// [345, 897, 568, 234] -> 2
+
+Console.Write("Введите число ");
+int[] GetArray(int size, int minValue, int maxValue)
+{
+int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+    res[i] = new Random().Next(minValue, +  1);
+    Console.Write($"{res[i]} ");
+    }
+    return ;
+}
+int[] array = GetArray(4, 100, 1000);
+Console.WriteLine();
+foreach (int el in array)
+{
+    if (el % 2 != 0)
+        continue;
+    ++++
+}
+Console.WriteLine();
+Console.WriteLine($"Количество четных чисел равно {sumEvenNumbers}");
--- Work5/Htask52/Program.cs
+++ Work5/Htask52/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// Например:
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+Console.Clear();
+int[] GetArray(int size, int minValue, int maxValue)
+{
+int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+    res[i] = new Random().Next(minValue, maxValue + 1);
+    Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+int[] array = GetArray(4, -100, 100);
+Console.WriteLine();
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(i%2 != 0)
+    sum = sum + array[i];
+}
+
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел, стоящих на нечетных позициях, равна {sum}");
