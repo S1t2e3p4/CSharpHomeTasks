7ae4e13c207549ad8b324b2843bd394a49dd4eab
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-11-04 13:54:32 +0300

    Создал папку для решения задачи2 по второму семинару

---
+++ Work2/Htask22/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа, или сообщает, что третьей цифры нет.
+// Например:
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Clear();
+ Console.WriteLine("Введите трёхзначное число: ");
+ int number = int.Parse(Console.ReadLine());
+ if ((number>99&&number<1000)||(number<-99&&number>-1000))
+ {
+ int Ostatok = number%100;
+ int betveenCount = Ostatok/10;
+ if (betveenCount<0)
+ {
+    betveenCount=betveenCount*-1;
+ }
+ Console.WriteLine (betveenCount);
+ }
+ else
+ {
+    Console.WriteLine ("Ошибка! Введено некорректное значение.");
+ }
+ 
