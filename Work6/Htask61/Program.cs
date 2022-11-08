1f617b0ec18f1e22a997d2a2c118de96eb2ec246
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-11-08 19:51:14 +0300

    Написал решение задачи 6.1 по шестому семинару

--- Work6/Htask61/Program.cs
+++ Work6/Htask61/Program.cs
@@ -3,20 +3,20 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 Console.Clear();
-Console.Write("Введите несколько чисел через пробел:  ");
-string = = .ReadLine();
+Console.Write("Введите несколько чисел через пробел: ");
+string numbers = Console.ReadLine();
 
-int = = CountPositiveNumbers(numbers);
+int result = CountPositiveNumbers(numbers);
 Console.Write($"Количество чисел больше нуля равно {result}.");
-int CountPositiveNumbers(string)
+int CountPositiveNumbers(string numbers)
 {
     string[] array = numbers.Split(' ');
-    0 = = 0;
+    int positivNumbers = 0;
     foreach(string el in array)
     {
-        int = = int.Parse(el);
+        int a = int.Parse(el);
         if (a>0) 
-        ++++;
+        positivNumbers++;
     }
     return positivNumbers;
 }
