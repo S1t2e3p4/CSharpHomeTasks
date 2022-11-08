fabbf9cff4deb752680f9705330aa0135ea0f862
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-11-08 19:19:13 +0300

    Написал решение задачи6.2 по шестому семинару

--- Work6/Htask61/Program.cs
+++ Work6/Htask61/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Например:
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Clear();
+Console.Write("Введите несколько чисел через пробел:  ");
+string = = .ReadLine();
+
+int = = CountPositiveNumbers(numbers);
+Console.Write($"Количество чисел больше нуля равно {result}.");
+int CountPositiveNumbers(string)
+{
+    string[] array = numbers.Split(' ');
+    0 = = 0;
+    foreach(string el in array)
+    {
+        int = = int.Parse(el);
+        if (a>0) 
+        ++++;
+    }
+    return positivNumbers;
+}
