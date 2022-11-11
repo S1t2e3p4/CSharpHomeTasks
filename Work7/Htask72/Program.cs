bce3475f7263736137968e5e24b03e8b9a3dc4b1
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-11-11 08:05:20 +0300

    Написал решение задачи 7.2 по седьмому семинару

--- Work7/Htask72/Program.cs
+++ Work7/Htask72/Program.cs
@@ -7,3 +7,59 @@
 // [1,7] -> такого числа в массиве нет ([1,7] это позиция элемента)
 
 Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+Console.Write("Введите, через запятую, позицию элемента (x,y): ");
+string position = Console.ReadLine();
+int RowIndex = -1;
+int ColumnIndex = -1;
+ParsePosition(position.Replace("[","").Replace("]",""));
+
+int[,] array = GetArray(rows, columns, 0,10);
+Element(array,RowIndex,ColumnIndex);
+Console.WriteLine();
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+        result[i,j] = new Random().Next(minValue,maxValue+1);
+        }
+    }
+return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void ParsePosition(string position)
+{
+    string[] strings = position.Split(',');
+    RowIndex = int.Parse(strings[0]);
+    ColumnIndex = int.Parse(strings[1]);
+}
+
+void Element (int [,] array, int i, int j)
+{
+
+if (i < array.GetLength(0) && j < array.GetLength(1))
+    {
+    Console.Write($"Позиция элемента [{i},{j}] имеет значение {array[i, j]}");
+    }
+else Console.Write($"Такого числа в массиве нет");
+}
