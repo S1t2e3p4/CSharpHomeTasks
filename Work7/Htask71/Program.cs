61c50e9ed1aaa2ad422db83bb214edaea41157d8
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-11-11 07:51:25 +0300

    Написал решение задачи 7.1 по седьмому семинару

--- Work7/Htask71/Program.cs
+++ Work7/Htask71/Program.cs
@@ -5,3 +5,35 @@
 // 8 7,8 -7,1 9
 
 Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+double[,] array = GetArray(rows, columns);
+PrintArray(array);
+
+double[,] GetArray(int m, int n)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+        result[i,j] = new Random().NextDouble();
+        }
+    }
+return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+}
