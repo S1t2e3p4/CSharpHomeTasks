b27c3a0ec7e5763745a0d43d36a62a6044b192c1
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-11-20 07:27:21 +0300

    Написал решение задачи 8.1 по восьмому семинару

--- Work8/Htask81/Program.cs
+++ Work8/Htask81/Program.cs
@@ -7,3 +7,71 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+Console.Clear();
+
+int[,] array;
+
+Run();
+
+void Run()
+{
+    FillArray(3, 4);
+    PrintArray();
+    Console.WriteLine();
+    SortArray();
+    PrintArray();
+}
+
+    void FillArray(int rowCount, int columnCount)
+{
+    array = new int[rowCount, columnCount];
+    Random rnd = new Random();
+
+    for (int i=0; i<rowCount; i++)
+    {
+        for(int j=0; j<columnCount; j++)
+            array[i,j] = rnd.Next(10, 99);
+            
+    }
+}
+
+void PrintArray()
+{
+    int rowCount = array.GetLength(0);
+    int columnCount = array.GetLength(1);
+
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < columnCount; j++)
+        {
+            if (array[i, j] < 10) Console.Write($"  {array[i, j]}");
+            else Console.Write($" {array[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int max = array[i, j];
+            int col = j;
+
+            for (int x = j; x < array.GetLength(1); x++)
+            {
+                if (array[i, x] > max)
+                {
+                    max = array[i, x];
+                    col = x;
+                }
+            }
+            int temp = array[i, j];
+            array[i, j] = max;
+            array[i, col] = temp;
+        }
+    }
+}
