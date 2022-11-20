d0d94053344439ec45c5c4b2a8583090292aee3c
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-11-20 07:43:58 +0300

    Написал решение задачи 8.2 по восьмому семинару

--- Work8/Htask82/Program.cs
+++ Work8/Htask82/Program.cs
@@ -7,4 +7,66 @@
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
 Console.Clear();
+int[,] array;
 
+Run();
+
+void Run()
+{
+    FillArray(5, 5);
+    PrintArray();
+    Console.WriteLine();
+    FindRowWitjMinimalSum();
+    
+}
+
+void FillArray(int rowCount, int columnCount)
+{
+    array = new int[rowCount, columnCount];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < columnCount; j++)
+            array[i, j] = rnd.Next(10, 99);
+    }
+}
+
+void PrintArray()
+{
+    int rowCount = array.GetLength(0);
+    int columnCount = array.GetLength(1);
+
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < columnCount; j++)
+        {
+            if (array[i, j] < 10) Console.Write($"  {array[i, j]}");
+            else Console.Write($" {array[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindRowWitjMinimalSum()
+{
+    int MinSum=int.MaxValue;
+    int row = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum+= array[i, j];
+        }
+        if (sum < MinSum)
+        {
+            MinSum = sum;
+            row = i;
+        }
+    }
+    Console.WriteLine($"Минимальная сумма в строке = {MinSum}");
+     Console.WriteLine($"Строка номер << {row+1} >> , является строкой с наименьшей суммой.");
+      Console.WriteLine($" ");
+}
