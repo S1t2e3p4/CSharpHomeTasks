282329d54fb8e06cb4a59b0f0748eba3b4a77bcf
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-11-28 20:34:27 +0300

    написал решение задачи 9.2 по девятому семинару

--- Work9/Htask92/Program.cs
+++ Work9/Htask92/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// Например:
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Run();
+
+void Run() 
+{
+    Console.WriteLine("Введите число < M >: ");
+    int numberM = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите число < N >: ");
+    int numberN = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Cумма натуральных элементов в промежутке от M до N равна: {Sum(numberN, numberM)}");
+
+}
+
+int Sum(int numberM, int numberN)
+{
+    if (numberM == numberN) return numberN;
+
+    return numberM + Sum(--numberM, numberN);
+}
