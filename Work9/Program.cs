17e1696d356912fdef26237a46a7feb5973f9dc8
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-11-28 20:04:24 +0300

    Создал папку для решения задачи 9.1 по девятому семинару

--- Work9/Program.cs
+++ Work9/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "4, 2"
+// N = 8 -> "8, 6, 4, 2,"
+Console.Clear();
