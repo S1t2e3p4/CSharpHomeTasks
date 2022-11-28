0be7c40e7c32f436d1264c03aaf9c12d4d66fe7b
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-11-28 20:49:42 +0300

    написал решение задачи 9.3 по девятому семинару

--- Work9/Htask93/Program.cs
+++ Work9/Htask93/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+Run();
+    void Run()
+{
+    Console.WriteLine("Введите неотрицательное число < m >: ");
+    int m = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите неотрицательное число < n >: ");
+    int n = int.Parse(Console.ReadLine());
+    Console.WriteLine($"А({m}, {n}) = {Akkerman((uint)m, (uint)n)}");
+}
+
+uint Akkerman(uint m, uint n)
+{
+    if (m == 0)  return n+1; 
+    if (m > 0 && n ==0)  return Akkerman(m - 1, 1); 
+    return Akkerman(m-1,Akkerman(m,n-1));
+}
